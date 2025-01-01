28f9782be3984142f0ec8bceff84eef4fb70e53e
Matt Gaunt-Seo <hello@gaunt.dev>
2025-01-01 11:29:18 -0800

    Add code install

--- steps/install-deps.sh
+++ steps/install-deps.sh
@@ -15,7 +15,12 @@ function installDeps() {
             sudo dnf install -y fedora-workstation-repositories dnf-plugins-core
             sudo dnf install -y "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
 
+            sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+            echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
             sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+            sudo dnf check-update
 
             # shellcheck disable=SC2046
             sudo dnf install -y $(cat ./dependencies/linux-packages.txt)
